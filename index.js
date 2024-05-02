c210f7e268002a11bf78be63c3838e17c51e3480
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-05-02 23:57:51 +0600

    my order api added

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
 
         const carCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('booking');
 
         app.get('/services', async (req, res) => {
             const services = await carCollection.find({}).toArray();
@@ -33,14 +34,42 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const options = {
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 }
             };
             const service = await carCollection.findOne(query, options);
             res.send(service);
         })
 
 
+        // booking api
 
+        app.post('/booking', async (req, res) => {
+            const newBooking = req.body;
+            const doc = {
+                name: newBooking.customerName,
+                date: newBooking.date,
+                email: newBooking.email,
+                dueAmount: newBooking.dueAmount,
+                image: newBooking.img
+            }
+            console.log(newBooking);
+            const result = await bookingCollection.insertOne(doc);
+            res.json(result);
+        });
+
+        app.get('/booking', async (req, res) => {
+            const result = await bookingCollection.find({}).toArray();
+            res.send(result);
+        })
+
+        app.get('/bookings', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
